9a2e3ebcd9ee6b050efe6e2e92b8dcfb800f85ba
Peter White <pwhitedeveloper@gmail.com>
2019-07-19 15:40:14 -0400

    user / users get routes

--- app/routes.py
+++ app/routes.py
@@ -13,24 +13,24 @@ def index():
 
 @app.route('/api/register', methods=['GET','POST'])
 def register():
-    # try:
-    token = request.headers.get('token')
+    try:
+        token = request.headers.get('token')
 
-    # decode the token back to a dictionary
-    data = jwt.decode(
-        token,
-        app.config["SECRET_KEY"],
-        algorithm=["HS256"]
-    )
+        # decode the token back to a dictionary
+        data = jwt.decode(
+            token,
+            app.config["SECRET_KEY"],
+            algorithm=["HS256"]
+        )
 
-    user = User(first_name=data["first_name"], last_name=data["last_name"], company=data["company"], username=data["username"], email=data["email"])
-    user.set_password(data["password"])
-    db.session.add(user)
-    db.session.commit()
+        user = User(first_name=data["first_name"], last_name=data["last_name"], company=data["company"], username=data["username"], email=data["email"])
+        user.set_password(data["password"])
+        db.session.add(user)
+        db.session.commit()
 
-    return jsonify({ "success" : "User {} created".format(data['username']) })
-    # except:
-    #     return jsonify({ "error": "Falid to create user" })
+        return jsonify({ "success" : "User {} created".format(data['username']) })
+    except:
+        return jsonify({ "error": "Falid to create user" })
 
 @app.route('/api/login', methods=['GET','POST'])
 def login():
@@ -52,3 +52,52 @@ def login():
 
     except:
         return jsonify({ "error" : "failed to login" })
+
+@app.route('/api/users', methods=["GET"])
+def getUsers():
+    try:
+        users = User.query.all()
+        usersJSON = []
+
+        for user in users:
+            usersJSON.append({
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "company": user.company,
+                "username": user.username,
+                "email": user.email,
+                "password_hash": user.password_hash
+            })
+
+        return jsonify(usersJSON)
+    except:
+        return jsonify({ "Error": "Cannot retrieve users" })
+
+@app.route('/api/user', methods=["GET"])
+def getUser():
+    try:
+        token = request.headers.get('token')
+
+        # decode the token back to a dictionary
+        data = jwt.decode(
+            token,
+            app.config["SECRET_KEY"],
+            algorithm=["HS256"]
+        )
+
+        userJSON = {}
+        user = User.query.filter_by(id = data["user_id"]).first()
+
+        if user:
+            userJSON["id"] = user.id
+            userJSON["first_name"] = user.first_name
+            userJSON["last_name"] = user.last_name
+            userJSON["company"] = user.company
+            userJSON["username"] = user.username
+            userJSON["email"] = user.email
+            userJSON["password_hash"] = user.password_hash
+
+        return jsonify(userJSON)
+    except:
+        return jsonify({"Error" : "Could not retrieve user"})
