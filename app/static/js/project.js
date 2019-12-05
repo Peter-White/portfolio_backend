f9f7104ad4e0a67802e87cce00fd75ca2064343e
Peter White <pwhitedeveloper@gmail.com>
2019-12-05 13:56:41 -0500

    img overlay added

--- app/static/js/project.js
+++ app/static/js/project.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   let $overlay = $("#overlay");
   let $card = $(".card");
+  let $cardImg = $(".card-img-top");
 
   // $("img").click(function(){ console.log($(this).attr("src")); });
   $("img").click(function(){
     $overlay.addClass("show");
+    $cardImg.attr("src", $(this).attr("src"));
   });
   $overlay.click(function(){
     $overlay.removeClass("show");
